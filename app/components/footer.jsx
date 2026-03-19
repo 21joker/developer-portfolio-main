a756aa145c527515df9b7abb92df7d4f0cdccbe0
21joker <62887636+21joker@users.noreply.github.com>
2026-03-19 08:29:53 +0800

    Update footer links to correct LinkedIn profile

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
-            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/abu-said-bd/" className="text-[#16f2b3]">Paul Arwin</Link>
+            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/paul-arwin-alda-766120277/" className="text-[#16f2b3]">Paul Arwin</Link>
           </p>
           <div className="flex items-center gap-5">
             <Link
@@ -21,7 +21,7 @@ function Footer() {
               className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
             >
               <IoStar />
-              <span>Star</span>
+              
             </Link>
             <Link
               target="_blank"
@@ -29,7 +29,7 @@ function Footer() {
               className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
             >
               <CgGitFork />
-              <span>Fork</span>
+              
             </Link>
           </div>
         </div>
